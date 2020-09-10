74be73285c27058f6f76cfa9903a0a647d15f9c8
masterful-principle-0059 <william.neal83@gmail.com>
2020-09-10 19:52:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,11 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   data.each_with_object({}) do |(key, val), organizer|
-    binding.pry
-    organizer
-  end
-  binding.pry
+    #binding.pry
+    val.each do |color, name|
+      binding.pry
+    end
+  organizer
+end
+binding.pry
 end
